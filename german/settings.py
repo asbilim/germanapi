b5fabe07040fa79551ab7a87e29149d0326ba965
asbilim <bilimdepaullilian@gmail.com>
2024-11-12 09:38:03 +0100

    sending the backend up

--- german/settings.py
+++ german/settings.py
@@ -45,11 +45,14 @@ MIDDLEWARE = [
 ]
 
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3002",
     "http://127.0.0.1:3003",
+    "https://german.devsplug.com",
 ]
 
 
